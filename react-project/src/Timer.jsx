a3bf3930b0b71a97c4314d0732e3c34dd81986ac
sjamillah <j.ssozi@alustudent.com>
2025-07-22 17:44:23 +0200

    added the stopping of the timer functionality

--- react-project/src/Timer.jsx
+++ react-project/src/Timer.jsx
@@ -1,9 +1,9 @@
 import React, { useRef, useState } from 'react'
 
 const Timer = () => {
-    const [timeLeft, setTimeLeft] = useState(0) // display time
+    const [timeLeft, setTimeLeft] = useState(0) // display the countdown
     const inputRef = useRef(0) // get the user's input
-    const timerRef = useRef(null) // store interval ID
+    const timerRef = useRef(null) // store interval ID so we can stop it
 
     const setTimer = () => {
         const userValue = parseInt(inputRef.current.value)
@@ -13,12 +13,15 @@ const Timer = () => {
             return;
         }
 
+        // set the initial countdown
         setTimeLeft(userValue)
 
+        // clears existing time before starting a new one
         if(timerRef.current) {
             clearInterval(timerRef.current)
         }
         
+        // start countdown
         timerRef.current = setInterval(() => {
             setTimeLeft(prev => {
                 if(prev <= 1) {
@@ -30,11 +33,18 @@ const Timer = () => {
             })
         }, 1000)
     }
+
+    const stopTimer = () => {
+        if(timerRef.current) {
+            clearInterval(timerRef.current)
+        }
+    }
   return (
     <div>
         <h1>Time left: {timeLeft}</h1>
       <input type="number" ref={inputRef} placeholder='Enter time in seconds'/>
       <button onClick={setTimer}>Set Timer</button>
+      <button onClick={stopTimer} style={{marginLeft: '10px'}}>Stop Timer</button>
     </div>
   )
 }
